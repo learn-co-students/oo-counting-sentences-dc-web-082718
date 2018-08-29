8d08ba8669c377d6fa8f25db4025a548ea616a41
github username <github email address>
2018-08-29 18:16:33 -0400

    Defined class methods that manipulate self.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,37 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self[-1,1] == "."
+      true
+    else
+      false
+    end
   end
 
   def question?
-
+    if self[-1,1] == "?"
+      true
+    else
+      false
+    end
   end
 
   def exclamation?
-
+    if self[-1,1] == "!"
+      true
+    else
+      false
+    end
   end
 
   def count_sentences
+    count = 0
 
+    self.split.each do |word|
+      if word.question? || word.exclamation? || word.sentence?
+        count += 1
+      end 
+    end
+    count
   end
 end
