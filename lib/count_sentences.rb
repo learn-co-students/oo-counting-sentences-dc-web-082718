13139c5aa753bfdc61be6558d050540661705d55
Jacob Berman <jacobberman1995@gmail.com>
2018-08-29 19:35:30 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,25 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
+    
+    self.split("").each.with_index do |char, index|
+      if 
+    end
+    array = self.split("."||"?"||"!")
+  
+    return array.length
 
   end
 end
